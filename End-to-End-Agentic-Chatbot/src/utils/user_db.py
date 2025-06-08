1d005e47543965f335a32597471bfc30357db1cb
Far-R <farzad.roozitalab@gmail.com>
2025-06-07 23:22:02 -0600

    remove project

--- End-to-End-Agentic-Chatbot/src/utils/user_db.py
+++
@@ -1,9 +0,0 @@
-
-
-def get_user_credentials() -> dict:
-    # Simulate fetching users from a database
-    return {
-        "farzad_rzt": "123",
-        "peter_parker": "letmein",
-        "admin": "securepass"
-    }
